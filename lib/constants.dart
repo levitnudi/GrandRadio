c277a35315da0c780d9cfcf53962bdff60e39394
Noton Inc <levitnudi@gmail.com>
2021-08-18 10:06:09 +0300

    Admob True
    Ads activated

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ final kCVSUrl =
 
     'https://docs.google.com/spreadsheets/d/e/2PACX-1vSAdm5-Fj0LpNj86upqP36bShFizETrseWzbcKj5ySx94-EvXTpo46vovs89GspmPx8exIAe4uSu4SN/pub?gid=0&single=true&output=csv';
 //'https://docs.google.com/spreadsheets/d/e/2PACX-1vTBO3FqidUiC5LZR6fYr2WiMXEz3Qgwpb6K9Fh_aEqhMaRb_a-eiroZhRSgFD9Vmiz6EXENrP-O6fPT/pub?output=csv';
-final kShowAd = false;
+final kShowAd = true;
 final kShowAdAfter = 10; // stations change
 
 
